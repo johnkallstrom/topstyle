0cc2603657ec3755638d1f2951bcc8f72af540d9
John Källström <jekallstrom@gmail.com>
2020-03-24 17:14:08 +0100

    fixes.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Home = () => {
   return (
     <div id='home'>
-      <h1>We deliver your order quickly and with no coronavirus! &#128567;</h1>
+      <h1>
+        We deliver your order quickly and with no coronavirus!{' '}
+        <span>&#128567;</span>
+      </h1>
     </div>
   );
 };
