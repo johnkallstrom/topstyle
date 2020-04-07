3e0bf7a45a718eb590e8c3b41441ed261a3ca3f3
johnkallstrom <jekallstrom@gmail.com>
2020-04-07 19:51:02 +0200

    changes

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 app.use(cors());
 app.use(express.json());
